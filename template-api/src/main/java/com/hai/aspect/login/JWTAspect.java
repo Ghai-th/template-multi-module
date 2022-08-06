4bce2e14770a8f9e09ec558f803e0043964b3dac
Ghai-th <439gfy>
2022-08-06 14:14:22 +0800

    不需要token的controller配置划分

--- template-api/src/main/java/com/hai/aspect/login/JWTAspect.java
+++ template-api/src/main/java/com/hai/aspect/login/JWTAspect.java
@@ -8,6 +8,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.CodeSignature;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -22,20 +23,27 @@ import javax.servlet.http.HttpServletRequest;
 @Aspect
 public class JWTAspect {
 
+    @Value("#{'${jwt.exclude}'.split(',')}")
+    String[] excludeController;
+
     @Pointcut("execution(* com.hai.controller.*.* (..))")
     public void pointcut(){
     }
 
     @Around("pointcut()")
     public Object doInvoke(ProceedingJoinPoint joinPoint){
-        Object proceed = null;
-        if (joinPoint.getSignature().toString().contains("LoginController")){
-            try {
-                return joinPoint.proceed();
-            } catch (Throwable e) {
-                throw new RuntimeException(e);
+        Object proceed;
+        /* 排除部分控制器 */
+        for (String exclude : excludeController) {
+            if (joinPoint.getSignature().toString().contains(exclude)){
+                try {
+                    return joinPoint.proceed();
+                } catch (Throwable e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
+        /* 解析token中的数据 */
         RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
         assert  requestAttributes != null;
         HttpServletRequest request = (HttpServletRequest) requestAttributes.resolveReference(RequestAttributes.REFERENCE_REQUEST);
@@ -48,12 +56,12 @@ public class JWTAspect {
         try {
             tokenParam = JSONObject.parseObject(JWTUtils.validateToken(token));
             if (tokenParam == null || tokenParam.size() == 0){
-                throw new RuntimeException();
+                return AjaxResponse.failed("错误的token");
             }
         } catch (Exception e){
             return AjaxResponse.failed("登录状态过期");
         }
-
+        /* 通过参数名称 注入 参数值 */
         Object[] args = joinPoint.getArgs();
         String[] paramNames = ((CodeSignature) joinPoint.getSignature()).getParameterNames();
         for (int i = 0; i < paramNames.length; i++) {
